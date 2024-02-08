39b5cab66d02c474cd7031e883d3c8a9034481a1
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 15:23:49 +0100

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        // Years divisible by 100 are not leap years 
+        // unless they are divisible by 400 
+        if (year % 100 === 0) {
+            return year % 400 === 0;
+        }
 
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
