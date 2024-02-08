f229b16b8ed4f528b428a69fb2da70279fa86e7a
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 15:04:51 +0100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    if (times < 0) {
+        return 'ERROR';
+    }
 
+    let result = '';
+
+    for (let i = 0; i < times; i++) {
+        result += str;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
