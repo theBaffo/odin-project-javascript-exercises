5cc63b32c0cff4ea203c706b4c01c7cb0f5f778d
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 18:24:58 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const calculatePersonAge = function(person) {
+    if (person.yearOfDeath) {
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return new Date().getFullYear() - person.yearOfBirth;
+    }
+}
 
+const findTheOldest = function(people) {
+    let oldestPerson;
+
+    people.forEach((person) => {
+        if (!oldestPerson) {
+            oldestPerson = person;
+        } else if (calculatePersonAge(person) > calculatePersonAge(oldestPerson)) {
+            oldestPerson = person;
+        }
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
