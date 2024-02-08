7f9bf2da826a4d370e8d70777c69cc84efebdda6
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 15:13:30 +0100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numsToRemove) {
+    for (let i = 0; i < numsToRemove.length; i++) {
+        let numToRemove = numsToRemove[i];
+        let idx = arr.indexOf(numToRemove);
 
+        while (idx !== -1) {
+            arr = arr.slice(0, idx).concat(arr.slice(idx + 1));
+            idx = arr.indexOf(numToRemove);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
