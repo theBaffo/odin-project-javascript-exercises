5b01056ed30cb476514673f1f6f58e79942cf333
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 18:17:13 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    if (typeof pos === 'string') {
+        pos = parseInt(pos)
+    }
 
+    if (pos < 0) {
+        return 'OOPS';
+    }
+
+    if (pos === 0) {
+        return 0;
+    }
+
+    if (pos === 1 || pos === 2) {
+        return 1;
+    }
+
+    return fibonacci(pos - 1) + fibonacci(pos - 2);
 };
 
 // Do not edit below this line
