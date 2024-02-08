c7e9d955773c5b1309d94a349311b719fa49f27a
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 18:12:34 +0100

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const removeNonAlphanumericCharacters = (input) => {
+    return input.replace(/[^a-zA-Z0-9]/g, '')
+}
 
+const palindromes = function (phrase) {
+    phrase = phrase.toLowerCase();
+    phrase = removeNonAlphanumericCharacters(phrase);
+
+    const reversePhrase = phrase.split('').reverse().join('');
+
+    console.log('phrase', phrase)
+    console.log('reversePhrase', reversePhrase)
+
+    return phrase === reversePhrase;
 };
 
 // Do not edit below this line
