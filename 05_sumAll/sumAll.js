ad2f55e59983e0aef12b53d87318fa314841aae4
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 15:18:53 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    // Parameter validation
+    if (typeof from !== 'number' || typeof to !== 'number') {
+        return 'ERROR';
+    }
 
+    if (from < 0 || to < 0) {
+        return 'ERROR';
+    }
+
+    // Parameters might be "swapped"
+    if (from > to) {
+        let tmp = from;
+        from = to;
+        to = tmp;
+    }
+
+    let sum = 0;
+
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
