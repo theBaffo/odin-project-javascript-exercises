9be492bc54e4ccd480da236d8f72b297401bce96
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 17:54:18 +0100

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = 0;
+
+  nums.forEach((num) => {
+    const parsedNum = parseInt(num);
+    total += parsedNum;
+  })
+
+  return total
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let total = 1;
 
+  nums.forEach((num) => {
+    const parsedNum = parseInt(num);
+    total *= parsedNum;
+  })
+
+  return total
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
