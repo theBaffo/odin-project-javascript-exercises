50d35335b1f225e204b12ba599deecb8d56ee263
Marco Giuliani <thebaffo@gmail.com>
2024-02-08 15:29:39 +0100

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const roundToDecimalPlaces = function(num, decimalPlaces) {
+  return Math.round(num * 10 ** decimalPlaces) / 10 ** decimalPlaces;
+}
+
+const convertToCelsius = function(temp) {
+  // (x − 32) × 5/9°C
+  const result = (temp - 32) * 5 / 9;
+  
+  return roundToDecimalPlaces(result, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // (x × 9/5 + 32)
+  const result = temp * 9 / 5 + 32;
+
+  return roundToDecimalPlaces(result, 1);
 };
 
 // Do not edit below this line
